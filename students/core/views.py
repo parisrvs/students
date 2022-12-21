682920bdc31923fe7eaf499823c7ce336bb2ea46
Parijat Srivastava <parisrvs@hotmail.com>
2022-12-22 03:23:54 +0530

    student serializer

--- students/core/views.py
+++ students/core/views.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
+# from rest_framework.response import Response
 from .serializers import StudentSerializer
 from .models import Student
 
