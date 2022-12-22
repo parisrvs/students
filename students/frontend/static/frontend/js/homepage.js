5c22d78a4c67749d0fe5e4cbdfc619f3900a8087
Parijat Srivastava <parisrvs@hotmail.com>
2022-12-22 16:38:38 +0530

    add result

--- students/frontend/static/frontend/js/homepage.js
+++ students/frontend/static/frontend/js/homepage.js
@@ -21,6 +21,20 @@ var add_student_gender_choices = [
     {"key": 'N', "value": "Non-Binary"},
 ]
 
+
+var add_result_subject_choices = [
+    {"key" :"PHY", "value": "Physics"},
+    {"key" :"CHE", "value": "Chemistry"},
+    {"key" :"BIO", "value": "Biology"},
+    {"key" :"EG1", "value": "English Lit."},
+    {"key" :"EG2", "value": "English Lang."},
+    {"key" :"MAT", "value": "Mathematics"},
+    {"key" :"HIS", "value": "History"},
+    {"key" :"GEO", "value": "Geography"},
+    {"key" :"CMP", "value": "Computer Science"}
+]
+
+
 function display_add_student_modal() {
     const add_student_modal_template = Handlebars.compile(document.querySelector('#addStudentModalHandlebars').innerHTML);
     const add_student_modal = add_student_modal_template({
@@ -52,13 +66,8 @@ function add_student() {
     request.open('POST', '/api/students/');
     request.setRequestHeader("X-CSRFToken", csrftoken);
 
-    disable();
-    prevent_default = true;
-
     request.onload = () => {
         const res = JSON.parse(request.responseText);
-        enable();
-        prevent_default = false;
         if (request.status === 400) {
             document.querySelectorAll(".errormessage").forEach(e => {
                 e.innerHTML = '';
@@ -84,4 +93,67 @@ function add_student() {
     data.append('mobile', student_mobile);
     request.send(data);
     return false;
+}
+
+
+function display_add_result_modal(student_id) {
+    const add_result_modal_template = Handlebars.compile(document.querySelector('#addResultModalHandlebars').innerHTML);
+    const add_result_modal = add_result_modal_template({
+        "subject_choices": add_result_subject_choices,
+        "student_id": student_id
+    });
+    document.querySelector("#addResultModal").innerHTML = add_result_modal;
+    document.querySelector("#addResultModalBtn").click();
+    focus_modal("addResultModal", "subject_name");
+    return false;
+}
+
+
+
+function add_result(student_id) {
+    let subject_name = document.querySelector("#subject_name").value.replace(/^\s+|\s+$/g, '');
+    let maximum_marks = document.querySelector("#maximum_marks").value.replace(/^\s+|\s+$/g, '');
+    let marks_obtained = document.querySelector("#marks_obtained").value.replace(/^\s+|\s+$/g, '');
+    let remarks = document.querySelector("#remarks").value.replace(/^\s+|\s+$/g, '');
+
+    if (!subject_name || !maximum_marks || !marks_obtained || !remarks) {
+        document.querySelector("#add_result_error").innerHTML = "Incomplete Form";
+        return false;
+    }
+
+    const csrftoken = getCookie('csrftoken');
+    const request = new XMLHttpRequest();
+    request.open('POST', `/api/students/${student_id}/results/`);
+    request.setRequestHeader("X-CSRFToken", csrftoken);
+
+    request.onload = () => {
+        const res = JSON.parse(request.responseText);
+        if (request.status === 400) {
+            document.querySelectorAll(".errormessage").forEach(e => {
+                e.innerHTML = '';
+            });
+            for (msg in res) {
+                if (msg === "non_field_errors") {
+                    document.querySelector("#add_result_error").innerHTML = res[msg];
+                }
+                let id = `feedback_${msg}`
+                if (document.getElementById(id))
+                    document.getElementById(id).innerHTML = res[msg][0];
+            }
+        } else if (request.status === 201) {
+            page = false;
+            load_students(page);
+            document.querySelector("#addResultModalCloseBtn").click();
+        }
+    };
+
+    const data = new FormData();
+    data.append('subject', subject_name);
+    data.append('max_marks', maximum_marks);
+    data.append('marks_obtained', marks_obtained);
+    data.append('remark', remarks);
+    request.send(data);
+    return false;
+
+
 }
